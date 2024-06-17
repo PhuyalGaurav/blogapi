9e0c14c3dcc6eb6a3e5f2ddc6d1f4080a9e1ea38
gauravphuyal <phuyalgaurav90@gmail.com>
2024-06-17 13:54:41 +0545

    Add User Views

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import PostListCreate, PostRetrieveUpdateDestroy
+from .views import PostListCreate, PostRetrieveUpdateDestroy, UserDetail, UserList
 
 urlpatterns = [
     path("<int:pk>/", PostRetrieveUpdateDestroy.as_view(), name="post_detail"),
     path("", PostListCreate.as_view(), name="post_list"),
+    path("users/<int:pk>/", UserDetail.as_view(), name="user_detail"),
+    path("users/", UserList.as_view(), name="user_list"),
 ]
